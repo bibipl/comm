a731d904aee52537c148b15226cfcf86faf6780e
Michał Kopiczyński <michal.kopiczynski@gmail.com>
2019-02-14 17:07:01 +0100

    Done: edit circle (front needs impovement), delete circle. Next : delete member form circle, add new member to the circle

--- src/main/java/mk/comm/Controller/CircleController.java
+++ src/main/java/mk/comm/Controller/CircleController.java
@@ -38,53 +38,153 @@ public class CircleController {
     public String addCircle (@AuthenticationPrincipal CurrentUser currentUser,
                             @PathVariable Long idGroup, Model model) {
         User user = currentUser.getUser();
-        if (user != null && idGroup > 0) { // we know there is and authentic user.
-            Group group = groupService.findById(idGroup);
-            if (group != null && group.getIdCommunity() > 0) { // we know group is not empte and belongs to existing community
-                Community community = communityService.findById(group.getIdCommunity());
-                if (community != null && community.getUserId() == user.getId()) { // we know community not null and use has rights
-                   // now we can start circle creation :
-                    // 1. get all members.
-                    List<Member> members = memberService.findAllByCommunityId(community.getId());
-                    // 2. Iterate all Circles form the group and add to List members in Circle
-                    List<Circle> circlesExisting= circleService.findAllByGroupIdOrderByNumberAsc(group.getId());
-                    List<Member> membersCircleBusy = new ArrayList<>();
 
-                    for (Circle circle : circlesExisting) {
-                        for (Member member : circle.getMembers()) {
-                            membersCircleBusy.add(member);
-                        }
-                    }
-                    // take ony ones that do not belong to any circle of the group :
-                    List <Member> membersCircleFree = new ArrayList<>();
-                    for (Member member : members) {
-                        if (!membersCircleBusy.contains(member)) {
-                            membersCircleFree.add(member);
-                        }
-
-                    }
-                    Circle circle = new Circle();
-                    model.addAttribute("idComm", community.getId());
-                    model.addAttribute("idGro", idGroup);
-                    model.addAttribute("circle", circle);
-                    model.addAttribute("members", membersCircleFree);
-                    model.addAttribute("iam", user);
-                    return "/circles/addCircle";
-                }
-            }
-        }
+          if (checkAdminCreentialGroup(user,idGroup)) {
+              Group group = groupService.findById(idGroup);
+              List<Member> membersCircleFree = membersNotInTheCircleYet(group);
+              Circle circle = new Circle();
+              model.addAttribute("idComm", group.getIdCommunity());
+              model.addAttribute("idGro", idGroup);
+              model.addAttribute("circle", circle);
+              model.addAttribute("members", membersCircleFree);
+              model.addAttribute("iam", user);
+              return "/circles/addCircle";
+          }
         if (idGroup >0 )
             return "redirect:/admin/groups/view/" + idGroup;
         return "redirect:admin/groups";
     }
+
     @PostMapping ("/add/{idGroup}")
     public String addCircleAction (@AuthenticationPrincipal CurrentUser currentUser,
                                    @PathVariable Long idGroup,
                                    @RequestParam(value = "circleMembers", required = false) long[] circleMembers,
                                    @ModelAttribute Circle circle, BindingResult result) {
+        User user = currentUser.getUser();
+        addCircleAction(user, idGroup, circleMembers, circle);
+        if (idGroup >0 )
+            return "redirect:/admin/groups/view/" + idGroup;
+        return "redirect:admin/groups";
+    }
 
-        // TODO check user credentials;
-        if (circleMembers != null) {
+
+    @GetMapping("/editCircle/{idCircle}")
+    public String editCircle (@AuthenticationPrincipal CurrentUser currentUser,
+                              @PathVariable Long idCircle, Model model) {
+        User user = currentUser.getUser();
+        if (checkAminCredentialCircle(user, idCircle)) {
+            Circle circle = circleService.findById(idCircle);
+            model.addAttribute("circle", circle);
+            return "/circles/editCircle";
+        }
+        return"/";
+    }
+
+    @PostMapping ("/editCircle")
+    public String editCircleAction (@AuthenticationPrincipal CurrentUser currentUser,
+                                   @ModelAttribute Circle circle, BindingResult result) {
+        User user = currentUser.getUser();
+        if (circle != null) {
+            int num = circle.getNumber();
+            if (num > 0) {
+                circle = circleService.findById(circle.getId());
+                circle.setNumber(num);
+                circleService.save(circle);
+            }
+            if (circle.getGroupId() > 0)
+                return "redirect:/admin/groups/view/" + circle.getGroupId();
+            return "redirect:admin/groups";
+        }
+        return "redirect:/";
+    }
+
+    @GetMapping("/deleteCircle/{idCircle}")
+    public String deleteCircle (@AuthenticationPrincipal CurrentUser currentUser,
+                                @PathVariable Long idCircle, Model model) {
+        User user = currentUser.getUser();
+        if (checkAminCredentialCircle(user, idCircle)) {
+            Circle circle = circleService.findById(idCircle);
+            model.addAttribute("circle", circle);
+            return "/circles/deleteCircle";
+        }
+        return"/";
+    }
+
+    @PostMapping ("/deleteCircle")
+    public String deleteCircleAction (@AuthenticationPrincipal CurrentUser currentUser,
+                                      @ModelAttribute Circle circle) {
+        // we pass here only circle id in the circle object.
+        User user = currentUser.getUser();
+        if (circle != null) {
+            circle = circleService.findById(circle.getId());
+            Long idGroup = circle.getGroupId();
+            if (idGroup > 0) {
+                if (checkAdminCreentialGroup(user, circle.getGroupId())) {
+                    circleService.delete(circle);
+                    return "redirect:/admin/groups/view/" + idGroup;
+                }
+            }
+        }
+        return "redirect:admin/groups";
+    }
+
+    @GetMapping("/changeResp/{idCircle}/{idMember}")
+    public String changeResp (@AuthenticationPrincipal CurrentUser currentUser,
+                              @PathVariable Long idCircle, @PathVariable Long idMember,
+                              Model model) {
+
+        User user = currentUser.getUser();
+        if (checkAminCredentialCircle (user, idCircle)) {
+            Circle circle = changeRespAction(idCircle, idMember);
+            return "redirect:/admin/groups/view/" + circle.getGroupId();
+        }
+        return "redirect:/";
+
+    }
+
+
+    // *** checks if user has right to change anything in the "circle".
+    // *** additionally confirms that community, group and circle are not empty and have not empty id to parent.
+        private boolean checkAminCredentialCircle (User user, Long idCircle) {
+        boolean credential = false;
+        if (idCircle > 0) {
+            Circle circle = circleService.findById(idCircle);
+            if (circle != null && circle.getGroupId() > 0) {
+                credential = checkAdminCreentialGroup(user, circle.getGroupId());
+            }
+        }
+        return credential;
+    }
+
+    // *** checks if user has right to change anything in the "group".
+    // *** additionally confirms that community and group are not empty and have not empty id to parent.
+    private  boolean checkAdminCreentialGroup (User user, Long idGroup) {
+        boolean credential = false;
+        if (idGroup > 0) {
+            Group group = groupService.findById(idGroup);
+            if (group != null && group.getIdCommunity() > 0) {
+                credential = checkAdminCreentialCommunity(user, group.getIdCommunity());
+            }
+        }
+        return credential;
+    }
+    // *** checks if user has right to change anything in the "community".
+    // *** additionally confirms that community ais not empty and have not empty id to parent.
+    private  boolean checkAdminCreentialCommunity (User user, Long idCommunity) {
+        boolean credential = false;
+        if (idCommunity > 0) {
+            Community community = communityService.findById(idCommunity);
+            if (community != null && user != null && user.getId() >0) {
+                credential = true;
+            }
+        }
+        return credential;
+    }
+
+    // with table of members' ids and id of the group (circle belongs to) and circle partly filled (in the form) creeates cicrcle with members
+    // it check user if has credential to make a change.
+    private void addCircleAction(User user, Long idGroup, long[] circleMembers, Circle circle) {
+        if ( checkAdminCreentialGroup(user, idGroup) && circleMembers != null) {
             Group group = groupService.findById(idGroup); // Może id group też Request Param przekazać ?
             int numOfCircles = circleService.countAllByGroupId(group.getId());
             List<Member> members = new ArrayList<>();
@@ -96,55 +196,53 @@ public class CircleController {
             circle.setResponsible(members.get(0).getId()); /// here to be taken by checkbox - save circle and go to choose responsible, with checked (0)
             circle.setMembers(members);
             circle.setGroupId(group.getId());
-            if (circle.getId() == null) circleService.save(circle); // no id's save not properly sorted we have to save 2x
+            if (circle.getId() == null)
+                circleService.save(circle); // no id's save not properly sorted we have to save 2x
             circleService.save(circle);
-
         }
-        if (idGroup >0 )
-            return "redirect:/admin/groups/view/" + idGroup;
-        return "redirect:admin/groups";
     }
 
-    @GetMapping("/changeResp/{idCircle}/{idMember}")
-    public String changeResp (@AuthenticationPrincipal CurrentUser currentUser,
-                              @PathVariable Long idCircle, @PathVariable Long idMember,
-                              Model model) {
-        User user = currentUser.getUser();
-        if (checkAminCredential (user, idCircle)) {
-            Circle circle = circleService.findById(idCircle);
-            if (idMember > 0) {
-                Member memberRespNew = memberService.findById(idMember);
-                if (memberRespNew != null) {
-                    if (memberRespNew.getMarried() >0 && memberRespNew.getSex() == 'K') {
-                        idMember = memberRespNew.getMarried();
-                    }
-                    circle.setResponsible(idMember);
-                    circleService.save(circle);
-                    return "redirect:/admin/groups/view/" + circle.getGroupId();
-                }
+
+    // ****With input group, returns all members from group that have not been assignet to any circle in the group yet.
+    private List<Member> membersNotInTheCircleYet(Group group) {
+
+        //1. All group's members
+        List<Member> members = memberService.findAllByCommunityId(group.getIdCommunity());
+        // 2. Iterate all Circles form the group and add to List members in Circle
+        List<Circle> circlesExisting = circleService.findAllByGroupIdOrderByNumberAsc(group.getId());
+        List<Member> membersCircleBusy = new ArrayList<>();
+
+        for (Circle circle : circlesExisting) {
+            for (Member member : circle.getMembers()) {
+                membersCircleBusy.add(member);
             }
         }
+        // take ony ones that do not belong to any circle of the group :
+        List<Member> membersCircleFree = new ArrayList<>();
+        for (Member member : members) {
+            if (!membersCircleBusy.contains(member)) {
+                membersCircleFree.add(member);
+            }
 
-        return "redirect:/";
+        }
+        return membersCircleFree;
     }
 
-    // *** checks if user has right to change anything in the "circle".
-    // *** additionally confirms that community, group and circle are not empty and have not empty id to parent.
-    private boolean checkAminCredential (User user, Long idCircle) {
-        boolean credential = false;
-        if (user != null && idCircle >0) {
-            Circle circle = circleService.findById(idCircle);
-            if (circle != null && circle.getGroupId() > 0) {
-                Group group = groupService.findById(circle.getGroupId());
-                if (group != null && group.getIdCommunity() > 0) {
-                    Community community = communityService.findById(group.getIdCommunity());
-                    if (community != null && community.getUserId() == user.getId()) {
-                        credential = true;
-                    }
+
+    // changes actual Responsible in the circle "idCircle" for a noew one "idMember"
+    private Circle changeRespAction(Long idCircle, Long idMember) {
+        Circle circle = circleService.findById(idCircle);
+        if (idMember > 0) {
+            Member memberRespNew = memberService.findById(idMember);
+            if (memberRespNew != null) {
+                if (memberRespNew.getMarried() >0 && memberRespNew.getSex() == 'K') {
+                    idMember = memberRespNew.getMarried();
                 }
+                circle.setResponsible(idMember);
+                circleService.save(circle);
+                return circle;
             }
         }
-        return credential;
+        return null;
     }
-
 }
